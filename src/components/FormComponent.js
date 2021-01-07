90cc3a2c96a719f7a44c79ba0f83f469413b9b42
Jhanvi Desai <jhanvisdesai@gmail.com>
2021-01-07 14:58:59 +0530

    Two block for sign in. Just need to adjust the sign up properly

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -4,23 +4,24 @@ import styled from 'styled-components'
 const BackgroundBox = styled.div`
 background-color: #96E1FF;
 height:50vh;
-width:90%
+width:50%;
 
 display:flex;
 justify-content: center;
 align-items: center;
 
-margin: 15rem 40rem;
+margin: 15rem auto;
 
+// margin: 15rem 40rem;
 position:relative;
 border-radius:23px;`
 ;
 
 const Box1= styled.div`
-background-color: #0025A4;
+background-color: #1977cc;
 height:100%;
 width:50%;
-positiom:absolute;
+position:absolute;
 left:0;
 top:0;
 
@@ -32,7 +33,9 @@ top:0;
     position: absolute;
     width: 100%;
     height: 100%;
-    background-color: black;
+    background-color: #fff;;
+
+    z-index: -200;
 }
 
 &::before{
@@ -46,10 +49,49 @@ top:0;
 }
 `;
 
+const Box2= styled.div`
+// background-color: #b5d3e7;
+background-color: white;
+height:100%;
+width:50%;
+position:absolute;
+right:0;
+top:0;
+
+border-radius:0 23px 23px 0;
+`;
+
+const Form=styled.form`
+color:white;
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+height:100%;
+padding: 0 4rem;
+
+z-index:100;
+
+`;
+const Input=styled.input`
+background-colour:#fff;
+border-radius:23px;
+padding: 1rem 2rem;
+`;
+
 function FormComponent(){
     return <>
     <BackgroundBox>
+
+        <Form>
+            <h1>Sign IN</h1>
+            <Input type="email" name="email" id="emailId" placeholder="Email"/>
+            <Input type="password" name="password" id="passwordId" placeholder="Password"/>
+            <a href="#">Forgot your Password?</a>
+            <button>Sign In</button>
+        </Form>
         <Box1 />
+        <Box2 />
 
     </BackgroundBox>
     </>
